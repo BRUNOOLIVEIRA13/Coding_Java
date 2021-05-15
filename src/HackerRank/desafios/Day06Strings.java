8af00dc03f24aec2463c51f00abdb8a0c0c1c46e
BRUNOOLIVEIRA13 <bruno.henrique0liveir4@gmail.com>
2021-05-15 11:33:34 -0300

    :heavy_plus_sign:  Created file of a new challenge!

---
+++ src/HackerRank/desafios/Day06Strings.java
@@ -0,0 +1,15 @@
+package HackerRank.desafios;
+
+/**
+ * Given a string, S , of length N that is indexed from 0 to N - 1,
+ * print its even-indexed and odd-indexed characters as 2 space-separated strings
+ * on a single line.
+ * @author Bruno
+ */
+
+public class Day06Strings {
+
+    public static void main(String[] args) {
+
+    }
+}
