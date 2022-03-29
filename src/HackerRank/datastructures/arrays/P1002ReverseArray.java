5f93842699257b124ca57645d2f0d32b1172fe09
brunoliveiradev <bruno.henrique0liveir4@gmail.com>
2022-03-28 23:09:42 -0300

    :tada:  Recursive call to reverse the array.

--- src/HackerRank/datastructures/arrays/P1002ReverseArray.java
+++ src/HackerRank/datastructures/arrays/P1002ReverseArray.java
@@ -15,22 +15,35 @@ class Result {
         int aux = 0;
         int length = array.size();
 
-        for (int i = 0; i < length/2; ++i) {
+        for (int i = 0; i < length/2; i++) {
             aux = array.get(length - i - 1);
             array.set(length - i - 1, array.get(i));
             array.set(i, aux);
+            System.out.println(i);
         }
         return array;
     }
 
     // Alternative method
-    static List<Integer> reverseString(List<Integer> array) {
+    static List<Integer> reverseArrayAlternative(List<Integer> array) {
         List<Integer> reversedArray = new ArrayList<>();
 
         for(int i = array.size() - 1; i >= 0; i--) {
           reversedArray.add(array.get(i));
         }
         return reversedArray;
+    }
+
+    // Método Recursivo
+    static void reverseArrayRecursive(List<Integer> array) {
+        // Condição base de parada da recursão
+        if(array == null || array.size() <= 1) return;
+        // Operação
+        Integer x = array.remove(0);
+        // Recursão
+        reverseArrayRecursive(array);
+        // Retorno — Fim da Recursão
+        array.add(x);
     }
 }
 
@@ -47,7 +60,8 @@ public class P1002ReverseArray {
                     .map(Integer::parseInt)
                     .collect(toList());
 
-            List<Integer> res = Result.reverseString(arr);
+            List<Integer> res = Result.reverseArrayAlternative(arr);
+//            Result.reverseArrayRecursive(arr);
 
             bufferedWriter.write(
                     res.stream()
@@ -55,6 +69,14 @@ public class P1002ReverseArray {
                             .collect(joining(" "))
                             + "\n"
             );
+
+//            Para fins didáticos se quiser usar com a recursão:
+//            bufferedWriter.write(
+//                    arr.stream()
+//                            .map(Object::toString)
+//                            .collect(joining(" "))
+//                            + "\n"
+//            );
 
             bufferedReader.close();
         }
