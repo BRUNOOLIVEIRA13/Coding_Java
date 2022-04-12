b4cabca5328c16c2e507763f4f9b4d4c273a5d5e
brunoliveiradev <bruno.henrique0liveir4@gmail.com>
2022-04-11 22:35:13 -0300

    :tada: Solved a challenge for Min and Max calculation in the array.

---
+++ src/HackerRank/datastructures/arrays/P1005MiniMaxSumArray.java
@@ -0,0 +1,43 @@
+package HackerRank.datastructures.arrays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class P1005MiniMaxSumArray {
+    /**
+     *  Find the minimum and maximum values that can be calculated by summing exactly four of the five integers
+     */
+    public static void miniMaxSum(List<Integer> arr) {
+        long min = arr.get(0);
+        long max = min;
+        long sum = min;
+
+        for (int i = 1; i < arr.size(); i++) {
+            sum += arr.get(i);
+
+            if(arr.get(i) < min) min=arr.get(i);
+
+            if (arr.get(i) > max) max = arr.get(i);
+        }
+        System.out.print((sum - max) + " " + (sum-min));
+    }
+}
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        P1005MiniMaxSumArray.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
