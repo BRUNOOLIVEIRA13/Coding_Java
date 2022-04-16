958dd6051305de3c10827c87eded0a9875ca12e7
brunoliveiradev <bruno.henrique0liveir4@gmail.com>
2022-04-16 20:20:00 -0300

    :tada: Solved time conversion challenge. AM/PM format to military (24-hour)

--- src/HackerRank/datastructures/strings/P1006TimeConversion.java
+++ src/HackerRank/datastructures/strings/P1006TimeConversion.java
@@ -2,15 +2,31 @@ package HackerRank.datastructures.strings;
 
 import java.io.*;
 import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
+import java.time.format.DateTimeFormatter;
 
+/**
+ * Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+ */
 public class P1006TimeConversion {
 
     public static String timeConversion(String s) {
-        //code here
-        LocalTime sevenThirty = LocalTime.parse("06:30").plus(1, ChronoUnit.HOURS);
+        if (s.endsWith("PM")) {
+            if (s.startsWith("12")) {
+                return s.substring(0, s.length() - 2);
+            } else {
+                LocalTime militaryTime = LocalTime.parse(s.substring(0, s.length() - 2)).plusHours(12);
 
-        return s;
+                return militaryTime.format(DateTimeFormatter.ISO_TIME);
+            }
+        } else {
+            if (s.endsWith("AM") && s.startsWith("12")) {
+                String newHour = s.replace(s.substring(0, 2), "00");
+
+                return newHour.substring(0, newHour.length() - 2);
+            } else {
+                return s.substring(0, s.length() - 2);
+            }
+        }
     }
 }
 
